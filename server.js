36b7b360a8ca4b475c395ba42ca914bde23d920b
theHparth <pphirpara@gmail.com>
2022-02-28 22:53:32 -0500

    char update

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var connect = require("connect");
-var http = require("http");
-
-var path = "";
-var app = connect().use(express.static(__dirname + path));
-
-http.createServer(app).listen(3000);
