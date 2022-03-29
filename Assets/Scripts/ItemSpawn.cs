a2bccbbfd98e9029c553eec5a531d8ff3bb56a6a
Parth <parthrg2080@gmail.com>
2022-03-28 22:43:03 -0400

    itemSpawnerScript creation with arrays of GameObject and transforms

---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform[] places;
+    public GameObject[] GoodItems;
+    public GameObject[] BadItems;
+}
