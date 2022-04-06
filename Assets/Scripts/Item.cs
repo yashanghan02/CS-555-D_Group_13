388014f87ed9711fd7ae88f7913686c93e36a026
Parth Bhingaradiya <67889827+ParthBhingaradiya@users.noreply.github.com>
2022-04-05 21:59:25 -0400

    Items data

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public bool isGood;
+
+    public GameObject[] itemModel;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Instantiate(itemModel[Random.Range(0,itemModel.Length-1)], this.transform);
+    }
+
+}
+    
