6318022165c4f82d2033f05fc3974244bdca72f9
Ajaykumar Lathiya <65893682+AjaykumarLathiya@users.noreply.github.com>
2022-04-12 22:58:42 -0400

    Player Controls modified

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -34,6 +34,7 @@ public class PlayerControls : MonoBehaviour
         if (b_walk)
         {
             transform.Translate(0, 0, walk_speed * Time.deltaTime);
+            SetDynamicSpeed();
         }
     }
     // Update is called once per frame
@@ -53,6 +54,74 @@ public class PlayerControls : MonoBehaviour
         }
 
         this.transform.position = Vector3.Lerp(this.transform.position,new Vector3(Positions[positionIndex].x,transform.position.y,transform.position.z),turnspeed*Time.deltaTime);
+    }
+
+    void SetDynamicSpeed()
+    {
+        if (Age>=90) {
+            setAnimBlend("Blend", 0f);
+            SetAnim(true, "Tired");
+            walk_speed = 3;
+            
+        }
+        if(Age < 90 && Age >= 80)
+        {
+            setAnimBlend("Blend", 0f);
+            SetAnim(true, "Tired");walk_speed = 4;
+            
+        }
+
+        if(Age<80 && Age>= 60)
+        {
+            setAnimBlend("Blend", .5f);
+            SetAnim(false, "Walk", true);
+            walk_speed = 5;
+           
+
+        }
+        if (Age < 60 && Age >= 40)
+        {
+            SetAnim(true, "Blending");
+            SetAnim(false, "Walk",true);
+          
+            setAnimBlend("Blend", .5f);
+            walk_speed = 7;
+           
+        }
+        if(Age<40 && Age >= 20)
+        {
+            SetAnim(true, "Walk", false) ;
+            setAnimBlend("Blend", 1);
+            walk_speed = 8;
+        }
+    }
+
+    
+
+    void SetAnim(bool trigger, string value,bool boolean=false)
+    {
+        if (!trigger)
+        {
+            foreach(var x in anim)
+            {
+                x.SetBool(value, boolean);
+            }
+        }
+        else
+        {
+            foreach (var x in anim)
+            {
+                x.SetTrigger(value);
+            }
+        }
+    }
+
+    void setAnimBlend(string value,float blendvalue)
+    {
+        foreach (var x in anim)
+        {
+            x.SetFloat(value, blendvalue);
+        }
     }
 
 
@@ -92,15 +161,16 @@ public class PlayerControls : MonoBehaviour
     {
         if (other.CompareTag("Collectible"))
         {
-            if (other.GetComponent<Item>().isGood)
+            var item = other.GetComponent<Item>();
+            if (item.isGood)
             {
-                Age--;
+                Age-=item.thisItemHealth;
             }
             else
             {
-                Age++;
+                Age += item.thisItemHealth;
             }
-
+            UIManager.instance.SendScore(Age);
             Destroy(other.gameObject);
         }
     }
