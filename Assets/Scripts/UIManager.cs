e67efb0417371e9e5b7913c0d1613aafbec1dad6
Parth <parthrg2080@gmail.com>
2022-04-14 22:19:40 -0400

    UI Controller script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject Home,GamePlay;
+    public TMPro.TextMeshProUGUI scoreTxt;
+    public static UIManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Home.SetActive(true);
+    }
+   public void StartGame()
+    {
+        Home.SetActive(false);
+        GamePlay.SetActive(false);
+    }
+
+    public void GameFinish()
+    {
+
+    }
+
+    public void SendScore(int value)
+    {
+        scoreTxt.text ="AGE: "+ value.ToString();
+    }
+}
