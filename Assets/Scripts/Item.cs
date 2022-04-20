471fef36c99ec59d7c0834935369993b949860ac
Ajaykumar Lathiya <65893682+AjaykumarLathiya@users.noreply.github.com>
2022-04-19 21:05:15 -0400

    Item.cs file is modified

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -9,14 +9,11 @@ public class Item : MonoBehaviour
 
     public GameObject[] itemModel;
     public int[] HealthAffect;
-    public int thisItemHealth;
     // Start is called before the first frame update
     private void Start()
     {
-        int randomVal = Random.Range(0, itemModel.Length - 1);
-        Instantiate(itemModel[randomVal], this.transform);
-        thisItemHealth = HealthAffect[randomVal];
-    }   
+        Instantiate(itemModel[Random.Range(0,itemModel.Length-1)], this.transform);
+    }
 
 }
     
