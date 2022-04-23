d55561749e8374cdc1b327f7189070eb15cf1af7
yashanghan02 <78460030+yashanghan02@users.noreply.github.com>
2022-04-22 20:55:33 -0400

    Game manager changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        Time.timeScale = 0;
+    }
+    public void startGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
