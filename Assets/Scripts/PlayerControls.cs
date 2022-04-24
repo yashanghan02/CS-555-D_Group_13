35f420cde3bdf16ec6ebac2dc8f4e3e92d0380e4
Parth Bhingaradiya <82073411+pg2080@users.noreply.github.com>
2022-04-24 12:57:30 -0400

    Revert "Player controls and rock prefab"
    This reverts commit 12e6ce91aafe3363786ec7d29489bb5448d2672b.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -28,8 +28,6 @@ public class PlayerControls : MonoBehaviour
     void Start()
     {
         positionIndex = 1; //middle
-        UIManager.instance.SendScore(Age);
-
     }
     void walk()
     {
@@ -125,28 +123,20 @@ public class PlayerControls : MonoBehaviour
             x.SetFloat(value, blendvalue);
         }
     }
-    public float distance;
+
 
     void checkSwipe()
     {
-        if (Vector3.Distance(initial, final) > distance)
+       
+        if(initial.x > final.x)
         {
-
-
-            if (initial.x > final.x)
-            {
-                Debug.Log("left");
-                GoLeft();
-            }
-            else
-            {
-                Debug.Log("right");
-                GoRight();
-            }
+            Debug.Log("left");
+            GoLeft();
         }
         else
         {
-            Debug.Log("Ignore Swipe");
+            Debug.Log("right");
+            GoRight();
         }
     }
 
@@ -174,44 +164,17 @@ public class PlayerControls : MonoBehaviour
             var item = other.GetComponent<Item>();
             if (item.isGood)
             {
-                if(Age> 20)
-                {
-                    Age -= item.thisItemHealth;
-                    if(Age <= 20)
-                    {
-                        Age = 20;
-                    }
-                }
-                else
-                {
-                    Age = 20;
-                }
-                GameManager.instance.UpdateScore(item.thisItemHealth);
+                Age-=item.thisItemHealth;
             }
             else
             {
                 Age += item.thisItemHealth;
-               // GameManager.instance.UpdateScore(-item.thisItemHealth);
             }
             UIManager.instance.SendScore(Age);
             Destroy(other.gameObject);
         }
-
-        if (other.CompareTag("Enemy"))
-        {
-            SetAnim(true, "Collision");
-            b_walk = false;
-            walk_speed = 0;
-            Destroy(other.gameObject);
-            Invoke(nameof(setGameOver), 2);
-        }
     }
 
-
-    void setGameOver()
-    {
-        UIManager.instance.ShowGameOver();
-    }
 
 }
 [System.Serializable]
