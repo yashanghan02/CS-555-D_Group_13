2291f5692ca265255c197ffd8a74664cd57ffa10
yashanghan02 <78460030+yashanghan02@users.noreply.github.com>
2022-04-24 13:03:36 -0400

    Game Manager Changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,6 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager instance;
-    public int Age;
-    public int Score;
-    public int bestScore;
 
     private void Awake()
     {
@@ -23,24 +20,6 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-    }
-
-    public int TrySetHighScore()
-    {
-        int value = PlayerPrefs.GetInt("BestScore");
-        if (value < Score)
-        {
-            PlayerPrefs.SetInt("BestScore", Score);
-            value = Score;
-        }
-        return value;
-        
-    }
-
-    public void UpdateScore(int value)
-    {
-        Score += value;
-        UIManager.instance.AddPoints(Score);
     }
     void Start()
     {
