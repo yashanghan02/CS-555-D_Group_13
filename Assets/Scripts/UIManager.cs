0410d7480d8d9c0c85131f87c20a939c1f7b487c
Ajaykumar Lathiya <65893682+AjaykumarLathiya@users.noreply.github.com>
2022-04-24 08:32:12 -0400

    UIManager file modified

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject Home,GamePlay;
+    public GameObject Home,GamePlay,GameOver;
     public TMPro.TextMeshProUGUI scoreTxt;
     public static UIManager instance;
+    public TMPro.TextMeshProUGUI[] BestScoreTxt,Points_txt;
     private void Awake()
     {
         instance = this;
@@ -16,21 +17,54 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         Home.SetActive(true);
+        fetchBestScore();
+      
+    }
+
+    public int fetchBestScore()
+    {
+        var value = PlayerPrefs.GetInt("BestScore");
+        foreach (var x in BestScoreTxt)
+        {
+            x.text = "Best Score: " + value;
+        }
+        return value;
     }
    public void StartGame()
     {
         Home.SetActive(false);
-        GamePlay.SetActive(false);
+        GamePlay.SetActive(true);
         GameManager.instance.startGame();
     }
 
     public void GameFinish()
     {
-
+        Debug.Log("GameFinish");
+        GameManager.instance.TrySetHighScore();
+        fetchBestScore();
     }
 
     public void SendScore(int value)
     {
         scoreTxt.text ="AGE: "+ value.ToString();
+    }
+
+    public void AddPoints(int value)
+    {
+        foreach(var x in Points_txt)
+        {
+            x.text = "Score: "+value.ToString();
+        }
+    }
+
+    public void RestartGame()
+    {
+        GameManager.instance.RestartGame();
+    }
+
+    public void ShowGameOver()
+    {
+        GameOver.SetActive(true);
+        GameFinish();
     }
 }
